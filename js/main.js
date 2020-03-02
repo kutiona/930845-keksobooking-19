fee898d5d0e8d145279ab686151aa99c33a12cf5
kutiona <50425837+kutiona@users.noreply.github.com>
2020-03-02 22:41:09 +0100

    location refactoring

--- js/main.js
+++ js/main.js
@@ -13,58 +13,71 @@ var MAX_OFFER_ROOMS = 4;
 var MAX_OFFER_GUESTS = 8;
 var OFFER_CHECKINS = ['12:00', '13:00', '14:00'];
 var OFFER_CHECKOUTS = ['12:00', '13:00', '14:00'];
-var OFFER_FEUTURES = [
-  ['washer', 'elevator', 'conditioner'],
-  ['wifi', 'dishwasher', 'parking'],
-  ['wifi', 'parking', 'washer', 'conditioner']
-];
-
+var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
 var pinList = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
+// Функция генерации случайного целого числа
 function getRandomInteger(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
-};
+}
 
 var getRandomValue = function (data) {
   var randomIndex = Math.floor(Math.random() * data.length);
   return data[randomIndex];
 };
 
+// Функция генерации массива строк случайной длины
+function getRandomArray(arr) {
+  var randomArray = [];
+  var copyArray = [];
+  for (var i = 0; i < arr.length; i++) {
+    copyArray[i] = arr[i];
+  }
+  var RandomArrayLength = getRandomInteger(1, arr.length);
+  for (i = 0; i < RandomArrayLength; i++) {
+    var randomIndex = getRandomValue(copyArray.length);
+
+    randomArray[i] = copyArray[randomIndex];
+    copyArray.splice(randomIndex, 1);
+  }
+  return randomArray;
+}
+
 // функция генерации случайных данных
 var generateAds = function (quantity) {
   var ads = [];
   for (var i = 0; i < quantity; i++) {
-    var location_x = getRandomInteger(START_LOCATION_X, END_LOCATION_X);
-    var location_y = getRandomInteger(START_LOCATION_Y, END_LOCATION_Y);
-    
+    var locationX = getRandomInteger(START_LOCATION_X, END_LOCATION_X);
+    var locationY = getRandomInteger(START_LOCATION_Y, END_LOCATION_Y);
+
     var ad = {
-    'author': {
-      'avatar': 'img/avatars/user0' + (i + 1) + '.png'
-    },
-    'offer': {
-      'title': 'Заголовок предложения'+ (i + 1),
-      'address': 'location_x,  location_y',
-      'price': getRandomInteger(0, MAX_OFFER_PRICES),
-      'type': getRandomValue(OFFER_TYPES),
-      'rooms': getRandomInteger(1, MAX_OFFER_ROOMS),
-      'guests': getRandomInteger(1, MAX_OFFER_GUESTS),
-      'checkin': getRandomValue(OFFER_CHECKINS),
-      'checkout': getRandomValue(OFFER_CHECKOUTS),
-      'features': getRandomValue(OFFER_FEUTURES),
-      'description': 'Строка с описанием'+ (i + 1),
-      'photos': 'http://o0.github.io/assets/images/tokyo/hotel' + (i + 1) + '.jpg'
-    },
-    'location': {
-      'x': location_x,
-      'y': location_y
-    }
-  };
+      'author': {
+        'avatar': 'img/avatars/user0' + (i + 1) + '.png'
+      },
+      'offer': {
+        'title': 'Заголовок предложения' + (i + 1),
+        'address': 'location_x,  location_y',
+        'price': getRandomInteger(0, MAX_OFFER_PRICES),
+        'type': getRandomValue(OFFER_TYPES),
+        'rooms': getRandomInteger(1, MAX_OFFER_ROOMS),
+        'guests': getRandomInteger(1, MAX_OFFER_GUESTS),
+        'checkin': getRandomValue(OFFER_CHECKINS),
+        'checkout': getRandomValue(OFFER_CHECKOUTS),
+        'features': getRandomArray(OFFER_FEATURES),
+        'description': 'Строка с описанием' + (i + 1),
+        'photos': 'http://o0.github.io/assets/images/tokyo/hotel' + (i + 1) + '.jpg'
+      },
+      'location': {
+        'x': locationX,
+        'y': locationY
+      }
+    };
 
     ads[i] = ad;
   }
@@ -94,6 +107,6 @@ var insertPins = function (ads) {
     fragment.appendChild(pinItem);
   }
   return fragment;
-}; 
+};
 
 pinList.append(insertPins(generateAds(ADS_QUANTITY)));
